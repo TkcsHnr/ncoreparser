c901fce40674c79577f7922ad45490f67d4c15b3
TkcsHnr <tkcshnr@gmail.com>
2024-01-29 16:24:52 +0100

    search function uses pages instead of number of torrents

--- ncoreparser/client.py
+++ ncoreparser/client.py
@@ -64,10 +64,10 @@ class Client:
     @_check_login
     # pylint: disable=too-many-arguments
     def search(self, pattern, type=SearchParamType.ALL_OWN, where=SearchParamWhere.NAME,
-               sort_by=ParamSort.UPLOAD, sort_order=ParamSeq.DECREASING, number=None):
+               sort_by=ParamSort.UPLOAD, sort_order=ParamSeq.DECREASING, pages=1):
         page_count = 1
         torrents = []
-        while ((page_count-1) * 25 == len(torrents)) and (number is None or len(torrents) < number):
+        while (page_count <= pages):
             url = URLs.DOWNLOAD_PATTERN.value.format(page=page_count,
                                                      t_type=type.value,
                                                      sort=sort_by.value,
@@ -79,11 +79,9 @@ class Client:
             except Exception as e:
                 raise NcoreConnectionError(f"Error while searhing torrents. {e}") from e
             new_torrents = [Torrent(**params) for params in self._page_parser.get_items(request.text)]
-            if number is None or len(new_torrents) == 0:
-                return torrents
             torrents.extend(new_torrents)
             page_count += 1
-        return torrents[:number]
+        return torrents
 
     @_check_login
     def get_torrent(self, id, **ext_params):
